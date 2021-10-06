2f7fec84e95f2bfbcc3ae8dd4ae17baa96cacee6
Paulius B <paulspremium@gmail.com>
2021-10-06 19:05:04 +0300

    triangle

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 "use strict";
+/**
+ * 1. Susikurkite TypeScript projektą
+ *
+ * 2. Parašykite programą, kuri padeda dirbti su trikampiais
+ *
+ * 3. Aprašykite klasę "Trikampis", kuri turėtu tris atributus
+ * simbolizuojančius visas tris trikampio kraštines (a, b, c).
+ *
+ * 4. Trikampio kraštinių reikšmės turi būti priskiriamos sukuriant
+ * objektą (konstruktoriuje).
+ *
+ * 5. Aprašykite metodą "spausdintiDuomenis()", kuris atspausdina
+ * visus trikampio duomenis (kraštines) į konsolę.
+ */
 var Triangle = /** @class */ (function () {
     function Triangle(a, b, c) {
-        this.printSidesLenght(a, b, c);
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.printSidesLenght(this.a, this.b, this.c);
     }
     Triangle.prototype.printSidesLenght = function (a, b, c) {
         console.log("Triangle sides lengths are : a - " + a + ", b - " + b + ", c - " + c);
@@ -9,3 +26,4 @@ var Triangle = /** @class */ (function () {
     return Triangle;
 }());
 var triangle = new Triangle(5, 7, 22);
+var triangle2 = new Triangle(6, 7, 100);
--- index.ts
+++ index.ts
@@ -1,11 +1,35 @@
+/**
+ * 1. Susikurkite TypeScript projektą
+ *
+ * 2. Parašykite programą, kuri padeda dirbti su trikampiais
+ *
+ * 3. Aprašykite klasę "Trikampis", kuri turėtu tris atributus
+ * simbolizuojančius visas tris trikampio kraštines (a, b, c).
+ *
+ * 4. Trikampio kraštinių reikšmės turi būti priskiriamos sukuriant
+ * objektą (konstruktoriuje).
+ *
+ * 5. Aprašykite metodą "spausdintiDuomenis()", kuris atspausdina
+ * visus trikampio duomenis (kraštines) į konsolę.
+ */
 class Triangle{
-constructor(a:number, b:number, c:number){
-this.printSidesLenght(a,b,c);
+    public a: number;
+    public b: number;
+    public c: number;
+   
+    constructor(a:number, b:number, c:number){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+
+        this.printSidesLenght(this.a,this.b,this.c);
 }
 
-printSidesLenght(a:number,b:number,c:number):void{
-console.log(`Triangle sides lengths are : a - ${a}, b - ${b}, c - ${c}`);
+    public printSidesLenght(a:number,b:number,c:number):void{
+        console.log(`Triangle sides lengths are : a - ${a}, b - ${b}, c - ${c}`);
 }
 }
 
 const triangle = new Triangle(5,7,22);
+const triangle2 = new Triangle(6,7,100);
+
